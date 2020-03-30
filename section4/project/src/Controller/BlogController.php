be8af87e2947bb8fb9468b3658be4932ba2742ca
Pavel <rogozhin.p@yandex.ru>
2020-03-30 18:31:49 +0300

    create first controller and database table

---
+++ section4/project/src/Controller/BlogController.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\BlogPost;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+    private const POSTS = [
+        [
+            'id' => 1,
+            'slug' => 'hello-world',
+            'title' => 'Hello world!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'another-post',
+            'title' => 'This is another post!'
+        ],
+        [
+            'id' => 3,
+            'slug' => 'last-example',
+            'title' => 'This is the last example'
+        ]
+    ];
+
+    /**
+     * @Route("/{page}", name="blog_list", defaults={"page": 5}, requirements={"page"="\d+"})
+     */
+    public function list($page = 1)
+    {
+        return new JsonResponse([
+            'page' => $page,
+            'data' => self::POSTS
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @param $id
+     * @return JsonResponse
+     */
+    public function post($id)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+        );
+    }
+
+    /**
+     * @Route("/{slug}", name="blog_by_slug")
+     * @param $slug
+     * @return JsonResponse
+     */
+    public function postBySlug($slug)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+        );
+
+
+    }
+
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
+    }
+}
