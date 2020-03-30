a2ea279e5c4dfa9da5c3383a569f6d8efec0b893
Pavel <rogozhin.p@yandex.ru>
2020-03-30 22:55:49 +0300

    solve problem with add item to database with post method

--- section4/project/src/Controller/BlogController.php
+++ section4/project/src/Controller/BlogController.php
@@ -15,23 +15,6 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class BlogController extends AbstractController
 {
-    private const POSTS = [
-        [
-            'id' => 1,
-            'slug' => 'hello-world',
-            'title' => 'Hello world!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'another-post',
-            'title' => 'This is another post!'
-        ],
-        [
-            'id' => 3,
-            'slug' => 'last-example',
-            'title' => 'This is the last example'
-        ]
-    ];
 
     /**
      * @Route("/{page}", name="blog_list", defaults={"page": 5}, requirements={"page"="\d+"})
@@ -56,22 +39,22 @@ class BlogController extends AbstractController
         );
     }
 
-    // /**
-    //  * @Route("/{slug}", name="blog_by_slug")
-    //  * @param $slug
-    //  * @return JsonResponse
-    //  */
-    // public function postBySlug($slug)
-    // {
-    //     return new JsonResponse(
-    //         self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
-    //     );
-    //
-    //
-    // }
+    /**
+     * @Route("/{slug}", name="blog_by_slug", methods={"GET"})
+     * @param $slug
+     * @return JsonResponse
+     */
+    public function postBySlug($slug)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+        );
+
+
+    }
 
     /**
-     * @Route("/add", name="blog_add", method={"POST"})
+     * @Route("/add", name="blog_add", methods={"POST"})
      */
     public function add(Request $request)
     {
